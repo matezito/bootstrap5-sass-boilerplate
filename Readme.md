5155f23e5ccb6e3f331d17b28ad526addf49610a
Juan Iriart <jeiriart@gmail.com>
2022-03-17 10:45:42 -0300

    first

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# How To
+
+Clone this repo, then, into the cloned forder, run ```npm install```
+
+In the src/scss/app.scss you put files of bootsrap to use (view the file for examples), same with the src/js/app.js for bootstrap js files.
+
+[see the docs](https://getbootstrap.com/docs/5.0/getting-started/webpack/)
+
+for deploys you need copy the assets folder (or css o js) into your project, the purpose of this boilerplate is modify bootstrap correctly
+
+When you make changes, run ```npm run build``` or you can run always ```npm run watch``` for reboot the webpack server automatically
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+
+module.exports = {
+    mode: "production",
+    entry: { //ADD ONE ENTRY FOR EACH PAGE WHIT THE SCSS AND JS FILE
+        app: ['./src/js/app.js', './src/scss/app.scss'],
+    },
+    output:{
+        path: path.resolve(__dirname),
+        filename: './assets/js/[name].min.js', //THIS BUILD DE BUNDLES WITH NAMES JS
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, exclude: /node_modules/,
+                use: { loader: "babel-loader" }
+            },
+            {
+                test: /\.(sass|scss)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            } 
+        ]
+    },
+    plugins: [new MiniCssExtractPlugin({ filename: './assets/css/[name].min.css' }) ], //THIS BUILD DE BUNDLES WITH NAMES CSS
+    optimization: {
+        minimizer: [
+            new UglifyJSPlugin({
+                cache: true,
+                parallel: true
+            }), 
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    }
+};
